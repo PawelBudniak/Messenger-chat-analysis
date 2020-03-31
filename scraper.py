fdd5713d8ffebd4d2675f943b785481b972531d8
Paweł Budniak <45400182+PawelBudniak@users.noreply.github.com>
2020-03-31 21:07:33 +0200

    comments added

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import os
 from bs4 import BeautifulSoup
 import json
+from collections.abc import Collection, Mapping
 
 MSG_PATH = R"D:\facebook-100002163210723\skladczarnuchow_9ZWCplP1iw"
 MSG_PATH2 = R"D:\facebook-100002163210723\TomekTrzeciak_kbNqf2UuEg"
@@ -55,12 +56,81 @@ class Scraper_html:
             json.dump(self.scrape(source_path), fp)
 
 
+def fix_encoding(var):
+    """
+    Fix broken messenger data encoding into proper utf-8
+    (the data was stored in utf-8 and decoded in latin1, so this functions performs a reverse operation)
+    
+    Arguments:
+        var {str or Collection} -- [If var is a collection, then the function is applied recursively, trying to find strings to fix, non str or Collection objects are ignored]
+    
+    Returns:
+        [str or Colletion] 
+    """
+    if isinstance(var, str):
+        return var.encode('latin1').decode('utf8')
+    elif isinstance(var, Mapping):
+        return type(var)({fix_encoding(k):fix_encoding(v) for k,v in var.items()})
+    elif isinstance(var, Collection):
+        return type(var)(fix_encoding(item) for item in var)
+
 class Scraper_json:
     def __init__(self):
         self.msg_types = ['photos', 'videos', 'audio_files', 'gifs', 'files', 'sticker', 'share']
 
+    def check_is_dir(self, a_path):
+        if not os.path.isdir(a_path):
+            raise Exception('Not a directory')
+    
+    def _get_files(self, path):
+        from re import match
+        files = [os.path.join(path, file) for file in os.listdir(path) if match('message_[0-9]+.json', file)]
+        return files        
+    
+    def is_garbage_msg(self, msg):
+        if msg['type'] == 'Subscribe' or msg['type'] == 'Unsubscribe':
+            return True
+        return False
+
+    def remove_garbage(self, df):
+        indices = df[(df['type'] == 'Subsribe') | (df['type'] == 'Unsubscribe') | (df['type'] == 'Call')].index
+        df.drop(columns = 'call duration', inplace = True, errors = 'ignore')
+        return df.drop(indices)
+
+    def scrape_to_df(self, path):
+        self.check_is_dir(path)
+        import pandas as pd
+
+        msg_files = self._get_files(path)
+        df = pd.DataFrame()
+
+        for file in msg_files:
+
+            with open(file, encoding='utf-8') as fp:
+                file_dict = json.load(fp)
+            
+            current_df = pd.DataFrame(file_dict['messages'])
+            current_df = self.remove_garbage(current_df)
+
+            current_df['content'] = current_df['content'].map(fix_encoding, na_action = 'ignore')
+            current_df['sender_name'] = current_df['sender_name'].map(fix_encoding, na_action = 'ignore')
+            if 'reactions'  in current_df:
+                current_df['reactions'] = current_df['reactions'].map(fix_encoding, na_action = 'ignore')
+
+            df = pd.concat([df, current_df], ignore_index=True, sort = False)
+
+        return df
+    
+    def scrape_to_csv(self, path, output_path):
+
+        df = self.scrape_to_df(path)
+        df.to_csv(output_path, encoding = 'utf-8')
+
+
+
 
     def _get_msg_type(self, msg_data):
+        '''Deprecated '''
         for atype in self.msg_types:
 
             if atype in msg_data:
@@ -71,14 +141,10 @@ class Scraper_json:
                 return atype
         return 'txt'
 
-    
-    def _get_files(self, path):
-        from re import match
-        files = [os.path.join(path, file) for file in os.listdir(path) if match('message_[0-9]+.json', file)]
-        return files        
-    
 
     def scrape(self, path):
+        ''' Deprecated'''
+        self.check_is_dir(path)
         msg_files = self._get_files(path)
         chat = {}
 
@@ -90,14 +156,13 @@ class Scraper_json:
                 #add participants to dict, so we don't have to check for new participants with every message
 
                 # fix broken messenger encoding
-                #TODO: explain
                 sender = participant['name'].encode('latin1').decode('utf8')
 
                 if sender not in chat:
                     chat[sender] = []
 
             for msg_data in file_dict['messages']:
-                if msg_data['type'] == 'Subscribe' or msg_data['type'] == 'Unsubscribe':
+                if self.is_garbage_msg(msg_data):
                     continue
 
                 date = msg_data['timestamp_ms']
@@ -113,43 +178,11 @@ class Scraper_json:
                 
                 chat[sender].append((txt, date, msg_type))
         return chat
-            
-
-    
+        
     def scrape_to_json(self, source_path, output_path):
+        ''' Deprecated '''
         with open(output_path, 'w', encoding='utf-8') as fp:
             json.dump(self.scrape(source_path), fp)
-
-
-    def scrape_to_df(self, path):  
-        import pandas as pd
-
-        msg_files = self._get_files(path)
-        df = pd.DataFrame()
-
-        for file in msg_files:
-
-            with open(file, encoding='utf-8') as fp:
-                file_dict = json.load(fp)
-
-            # fix broken messenger encoding
-            #TODO: explain
-            for p in file_dict['participants']:
-                p['name'] = p['name'].encode('latin1').decode('utf8')
-
-            for msg in file_dict['messages']:
-                if 'content' in msg:
-                    msg['content'] = msg['content'].encode('latin1').decode('utf8')
-                msg['sender_name'] = msg['sender_name'].encode('latin1').decode('utf8')
-
-            df = pd.concat( [ df, pd.DataFrame(file_dict['messages']) ], ignore_index=True, sort = True)
-
-        return df
-    
-    def scrape_to_csv(self, path, output_path):
-        df = self.scrape_to_df(path)
-        df.to_csv(output_path, encoding = 'utf-8')
-
 
 
 
--- stats_pandas.py
+++ stats_pandas.py
@@ -10,6 +10,8 @@ import pandas as pd
 import calendar
 import warnings
 
+class NoReactionsError(Exception): pass
+
 def load_from_path(path):
     import scraper
     s = scraper.Scraper_json()
@@ -19,6 +21,15 @@ def filter_nick_changes(chat_df):
     return chat_df[chat_df['content'].map(lambda msg: pd.isna(msg) or ( ('set the nickname' not in msg) and ('set his own nickname' not in msg)))]
 
 def get_msg_stats(chat_df):
+    """
+    General chat statistics
+    
+    Arguments:
+        chat_df {pd.DataFrame} -- main chat df returned by load_from_path()
+    
+    Returns:
+        dict -- {participant: list[total_message_count, average_message_length, total_characters_sent]}
+    """
     msg_stats = {}
 
     for sender in chat_df['sender_name'].unique():
@@ -36,6 +47,20 @@ def get_msg_stats(chat_df):
     return msg_stats
 
 def get_word_counts(chat_df, filter_participants_names = False, exclude_words = None, min_len = 1):
+    """
+    Calculate how many times each word has been used by each participant
+    
+    Arguments:
+        chat_df {pd.DataFrame} -- main chat df returned by load_from_path()
+    
+    Keyword Arguments:
+        filter_participants_names {bool} -- if True every participant name or surname is filtered (they usually clutter the counts) (default: {False})
+        exclude_words {Collection} -- strings in the Collection are filtered out (default: {None})
+        min_len {int} -- only words higher or equal to min_len are counted (default: {1})
+    
+    Returns:
+        dict -- {participant: dict{word: count}}
+    """
     senders = [sender for sender in chat_df['sender_name'].unique()]
     counts ={sender: {} for sender in senders}
     if filter_participants_names: 
@@ -62,33 +87,47 @@ def get_word_counts(chat_df, filter_participants_names = False, exclude_words = 
                     else:
                         counts[row['sender_name']][word] = 1
 
+    for sender in senders:
+        if not counts[sender]:
+            counts.pop(sender) # only include non-empty dictionaries (some may have had only words shorter than the minimal length)
+
     #sort words by descending counts, only include non-empty dictionaries (some may have had only words shorter than the minimal length)
-    counts = {sender: {k:v for k,v in sorted(counts[sender].items(), key = lambda item: item[1], reverse = True) if v} for sender in counts}
+    counts = {sender: {k:v for k,v in sorted(counts[sender].items(), key = lambda item: item[1], reverse = True)} for sender in counts}
     return counts
 
+
 def get_msg_types(chat_df, include_txt = False):
+    """Calculate how many messages of each type has been sent by each participant
+    
+    Arguments:
+        chat_df {pd.DataFrame} -- main chat df returned by load_from_path()
+    
+    Keyword Arguments:
+        include_txt {bool} -- if True then text messages are included as a type (messes up pie charts) (default: {False})
+    
+    Returns:
+        dict -- {participant: dict{type: count}}
+    """
     msg_types = ['photos', 'videos', 'audio_files', 'gifs', 'files', 'sticker', 'share']
     type_counts_by_sender = {}
 
     for sender in chat_df['sender_name'].unique():
         sender_df = chat_df.loc[chat_df['sender_name'] == sender]
         type_counts = {atype: 0 for atype in msg_types if atype in sender_df and sender_df[atype].count() != 0}
-        #pure_text_msgs = len(sender_df) 
-
 
         for atype in msg_types:
-            if atype not in sender_df:
+            if atype not in type_counts:
                 continue
             for alist in sender_df[atype].dropna():
                 #it's possible to have multiple multimedia files in a msg, so we want to count all of them
                 type_counts[atype] += len(alist)
 
-        type_counts_by_sender[sender] = type_counts
-    
         if include_txt == True:
-            type_counts_by_sender[sender]['txt'] = sender_df['content'].count()
-        #type_counts_by_sender[sender]['txt'] = pure_text_msgs
+            type_counts['txt'] = sender_df['content'].count()
 
+        if type_counts: #ignore senders who have written no messages of the desired types - in their case the type_counts dict is empty
+            type_counts_by_sender[sender] = type_counts
+    
         
     return type_counts_by_sender
 
@@ -100,7 +139,18 @@ def epoch_to_date(epoch_series, timezone = 'CET'):
     return dates
 
 def groupby_date(chat_df, frequency = 'M'):
-    ''' Groups number of messages by dates, sampling with the specified frequency (a pandas DateOffset) '''
+    """
+    Groups number of messages by dates, sampling with the specified frequency 
+    
+    Arguments:
+        chat_df {pandas.DataFrame} -- main chat df returned by load_from_path()
+
+    Keyword Arguments:
+        frequency {str} -- pandas DateOffset (default: {'M'})
+    
+    Returns:
+        pd.Series -- Series of msg counts indexed by time intervals
+    """
     dates = epoch_to_date(chat_df['timestamp_ms'])
     #since groupby groups by index
     dates.index = dates
@@ -108,13 +158,25 @@ def groupby_date(chat_df, frequency = 'M'):
     return dates   
 
 def groupby_time(chat_df, interval = 'M', interval_names = True):
-    ''' 
+    """
     Groups number of messages by the specified time interval 
     Different from groupby_date, for example: March 2013 is counted the same as March 2014 here if the interval is specified as 'M' (month)
-    Intervals: Y- year, M - month, W - weekday, D - day, H - hour
-    '''
+
+    Arguments:
+        chat_df {pd.DataFrame} -- main chat df returned by load_from_path()
+    
+    Keyword Arguments:
+        interval {str} -- Intervals: 'Y'- year, 'M' - month, 'W' - weekday, 'D' - day, 'H '- hour (default: {'M'})
+        interval_names {bool} -- valid for M and W intervals, if True str names are used instead of eg. 0 for Monday (default: {True})
+    
+    Returns:
+        pd.DataFrame  
+    """
+
     interval = interval.upper()
     functions = {'Y': lambda x: x.year, 'M': lambda x: x.month, 'W': lambda x: x.weekday, 'D': lambda x: x.day, 'H': lambda x: x.hour}
+    if interval not in functions:
+        raise ValueError('Invalid interval')
 
     times = epoch_to_date(chat_df['timestamp_ms'])
     #since groupby groups by index
@@ -126,12 +188,24 @@ def groupby_time(chat_df, interval = 'M', interval_names = True):
         times.index = times.index.map(lambda d: calendar.day_name[d])
     return times
 
-def get_kurwa_coefficients(word_counts, msg_stats, odmiana = False):
+def get_kurwa_coefficients(word_counts, msg_stats, count_related = False):
+    """Calculate kurwa_count/message_count for each participant
+
+    Arguments:
+        word_counts {dict} -- returned from get_word_counts()
+        msg_stats {dict} -- returned from get_msg_stats(), used to get message counts
+    
+    Keyword Arguments:
+        count_related {bool} -- if True related words, such as the plural form, are also counted (default: {False})
+    
+    Returns:
+        dict -- participant:coefficient
+    """
     coeffs = {}
     kurwas = {sender: 0 for sender in word_counts}
 
     for sender in word_counts:
-        if odmiana is True:
+        if count_related is True:
              for word in word_counts[sender]:
                 if 'kurw' in word or 'kurew' in word:
                     kurwas[sender] += word_counts[sender][word]
@@ -150,12 +224,26 @@ def get_kurwa_coefficients(word_counts, msg_stats, odmiana = False):
     return coeffs
 
 def get_profanity_coefficients(word_counts, msg_stats, ignore_kurwas = False):
+    """
+    Calculate profanity_count/message_count for each participant
+    
+    Arguments:
+        word_counts {dict} -- returned from get_word_counts()
+        msg_stats {dict} -- returned from get_msg_stats(), used to get message counts
+    
+    Keyword Arguments:
+        ignore_kurwas {bool} -- if True then every 'kurwa' related word is not counted (default: {False})
+    
+    Returns:
+        dict -- participant:profanity_coefficient
+    """
     import profanity
     coeffs = {}
 
     for sender in word_counts:
         total = 0
         for vulgarism in profanity.profanity:
+            
             if ignore_kurwas:
                 if 'kurw' in vulgarism or 'kurew' in vulgarism:
                     continue
@@ -168,12 +256,22 @@ def get_profanity_coefficients(word_counts, msg_stats, ignore_kurwas = False):
     return coeffs
 
 def total_reacts(chat_df):
+    """
+    Calculate how often each emoji was used 
+    
+    Arguments:
+        chat_df {pd.DataFrame} -- main chat df returned by load_from_path()
+  
+    Returns:
+        dict -- reaction: count
+    """
+    if 'reactions' not in chat_df:
+        raise NoReactionsError('No reactions have been made in the chat')
     react_types = {}
 
     for reacts in chat_df['reactions'].dropna():
         for react in reacts:
-            #fix encoding
-            emoji = react['reaction'].encode('latin1').decode('utf8')
+            emoji = react['reaction']
             if emoji in react_types:
                 react_types[emoji] += 1
             else:
@@ -181,7 +279,11 @@ def total_reacts(chat_df):
     return react_types
 
 def reaction_stats(chat_df):
-    ''' Returns: a tuple (pd.DataFrmae - reactions_made, pd.DataFrame - reactions_gotten) '''
+    ''' Returns: a tuple (pd.DataFrame - reactions_made, pd.DataFrame - reactions_gotten) '''
+
+    if 'reactions' not in chat_df:
+        raise NoReactionsError('No reactions have been made in the chat')
+
     filtered_df = chat_df.dropna(subset = ['reactions'])
     senders = chat_df['sender_name'].unique()
     reactions_gotten = {sender: {} for sender in senders}
@@ -189,13 +291,12 @@ def reaction_stats(chat_df):
 
     for index, row in filtered_df.iterrows():
         msg_sender = row['sender_name']
-        #reactions_gotten[msg_sender]['total'] += len(row['reactions'])
         
         for react in row['reactions']:
-            #fix encoding
-            actor = react['actor'].encode('latin1').decode('utf8')
-            react_emoji = react['reaction'].encode('latin1').decode('utf8')
-            
+
+            actor = react['actor']
+            react_emoji = react['reaction']
+
             if react_emoji not in reactions_gotten[msg_sender]:
                 reactions_gotten[msg_sender][react_emoji] = 1
             else:
@@ -205,10 +306,9 @@ def reaction_stats(chat_df):
                 reactions_made[actor][react_emoji] = 1
             else:
                 reactions_made[actor][react_emoji] += 1
-            #reactions_made[actor]['total'] += 1
             
-    reactions_gotten = pd.DataFrame(reactions_gotten).T
-    reactions_made = pd.DataFrame(reactions_made).T
+    reactions_gotten = pd.DataFrame(reactions_gotten).T.fillna(0)
+    reactions_made = pd.DataFrame(reactions_made).T.fillna(0)
     
     n_emojis = len(reactions_gotten.columns)
 
@@ -221,25 +321,34 @@ def reaction_stats(chat_df):
     return (reactions_made, reactions_gotten)
     
 def react_percents(df, msg_stats):
+    """
+    Amount of each reaction received by each participant relative to his message count
+    
+    Arguments:
+        df {pd.DataFrame} -- reactions received df (returned from reaction_stats())
+        msg_stats {dict} -- returned from get_msg_stats()
+    
+    Returns:
+        pd.DataFrame
+    """
     percents = df.copy()
     for index, row in percents.iterrows():
         n_msgs = msg_stats[index][0]
         percents.loc[index, :-1] = row[:-1]/n_msgs 
-    percents.fillna(0)
     #convert to str with '%' signs, ignore the last 'most_received' column
     percents[percents.columns[:-1]] = percents[percents.columns[:-1]].fillna(0).applymap(lambda x: f'{x:.2%}')
     return percents
 
-def most_reactions(df, title, emoji, percent = True):
+def most_reactions(df, title, emoji, percent):
     """Prints the 1st and 2nd person who received most of the specified emoji as a reaction
     
     Arguments:
-        df {pd.DataFrame} -- reactions df
+        df {pd.DataFrame} -- reactions received/percents df
         title {str} -- title for the person, eg. 'funniest' for laughing emoji
         emoji {str} -- one of the facebook reactions in utf-8   
     
     Keyword Arguments:
-        percent {bool} -- [indicate whether a df with numeric values or str percentages was passed] (default: {True})
+        percent {bool} -- indicate whether a df with numeric values or str percentages was passed
     """    
     if percent:
         #cast the str percentages to float so that max index can be computed
